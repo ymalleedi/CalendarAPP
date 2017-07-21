84cbda2382a84ff5b9ba544e1741ad3d45c7105c
ymalleedi <30351363+ymalleedi@users.noreply.github.com>
2017-07-21 15:01:06 -0400

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class TaskManager
 {
   public static void Initialize()
   {
+    Scanner console = new Scanner(System.in);
     System.out.println("Welcome to the Task Manager");
     System.out.print("Do you have an existing file? (Y/N)");
     
