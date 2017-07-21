ce71bbf7e4fdbe96d6cb3c6b6ddaae8cb01a76e7
ymalleedi <30351363+ymalleedi@users.noreply.github.com>
2017-07-21 15:42:28 -0400

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -9,6 +9,11 @@ public class TaskManager
     System.out.print("Do you have an existing file? (Y/N)");
     char r1 = console.next().toUpperCase().charAt(0);
     //if the user wants to read a file then we must call another method.
+    
+    /**
+    Here we have two options, we can learn to use the ArrayList and use it to store our Tasks, or we can restrict the user to a predefined
+    amount of tasks. So that would mean the user can only create 1000 tasks, view them in one session.
+    */
     if( r1 == 'Y')
       readFromFile(console); // this would have to return an existing task array.
     
