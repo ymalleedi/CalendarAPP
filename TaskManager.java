1f8d645398924fc100cb6e691cbc03c3e461cbac
ymalleedi <30351363+ymalleedi@users.noreply.github.com>
2017-07-21 15:06:15 -0400

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -5,11 +5,14 @@ public class TaskManager
   {
     Scanner console = new Scanner(System.in);
     System.out.println("Welcome to the Task Manager");
+    //We are going to have to error handle this.
     System.out.print("Do you have an existing file? (Y/N)");
-    
+    char r1 = console.next().toUpperCase().charAt(0);
+    //if the user wants to read a file then we must call another method.
+    if( r1 == 'Y')
+      readFromFile(console); // this would have to return an existing task array.
     
   }
-  
   
   
   public static void main (String[] args)
