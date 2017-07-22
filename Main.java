d4b29afb4ca3c37731b107614d7fd894878cecaf
ymalleedi <30351363+ymalleedi@users.noreply.github.com>
2017-07-22 14:34:18 -0400

    readFromFile method almost complete

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 import java.io.*;
 import java.util.*;
+
+/**
+ * This application serves as a task manager.
+ * @author Yaswanth Malleedi
+ */
 public class Main {
 
     public static void Initialize(Scanner console)
@@ -47,10 +52,10 @@ public class Main {
     {
         boolean check2 = false;
         Scanner FileScanner = null;
-        ArrayList<Task> taskList = null;
+        ArrayList<Task> taskList = new ArrayList<Task>();
         while(!check2)
         {
-            System.out.print("Enter your file name: ");
+            System.out.print("Enter your file name: "); // note to self the file must be in the root directory
             String fileName = console.nextLine();
 
             try
@@ -65,6 +70,37 @@ public class Main {
             }
         }
 
+        // if it got here then we have a file. Now we need to read it.
+        /*
+        We need to come up with a way to do this. One way is to program one scanner to read a
+        a delimiter.
+         */
+
+
+        //set scanner to use @nextTask@ as delimiter.
+        FileScanner.useDelimiter("@nextTask@\\s+");
+
+
+        while(FileScanner.hasNext())
+        {
+            //read the task
+            String currTask = FileScanner.next();
+            Scanner LineScanner = new Scanner(currTask);
+            //first line is the task title
+            String taskTitle = LineScanner.nextLine();
+            String taskDesc = LineScanner.nextLine();
+            int mm = LineScanner.nextInt();
+            int dd = LineScanner.nextInt();
+            int yy = LineScanner.nextInt();
+
+            //create the task in the array
+            
+            LineScanner.close();
+        }
+
+
+
+
         return taskList;
 
 
@@ -74,6 +110,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
+
         Initialize(console);
         console.close();
 
